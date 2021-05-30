c22f2765c4f37113dcc02906a0b912516a461db3
Dmitriy <dimalie@live.com>
2021-05-30 11:16:53 +0300

    ModalWindow

--- js/script.js
+++ js/script.js
@@ -38,4 +38,93 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-});
+
+    //Timer
+    const time = '2021-06-01';
+
+    function getDateRemainding(endtime) {
+        const RemaindDate = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(RemaindDate / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(RemaindDate / (1000 * 60 * 60)% 24),
+              minutes = Math.floor(RemaindDate / (1000 / 60 )% 60),
+              seconds = Math.floor((RemaindDate / 1000)% 60);
+
+        return {
+            'total': RemaindDate,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    
+    function getElrmrnts(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days'), 
+              hours = timer.querySelector('#hours'), 
+              minutes = timer.querySelector('#minutes'), 
+              seconds = timer.querySelector('#seconds');
+        const timeInterval = setInterval(setDate, 1000);
+
+        setDate();
+        function setDate() {
+            const t = getDateRemainding(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+        
+            if (t.RemaindDate <= 0) {
+            clearInterval(timeInterval);
+            }
+        }
+    }
+    getElrmrnts('.timer', time);
+
+    //modal
+
+    const buttons = document.querySelectorAll('[data-modal]'),
+          exit = document.querySelector('[data-close]'),
+          modal = document.querySelector('.modal');
+
+    function openModal() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = "hidden";
+      clearInterval(modalTimerId);
+    }
+
+    buttons.forEach((i)=> {
+        i.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        document.body.style.overflow = "";
+    }
+
+    modal.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target === modal || target === exit) {
+            closeModal();
+        }
+    });
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+//авто запуск через время 
+    const modalTimerId = setTimeout(openModal, 9000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+ });
